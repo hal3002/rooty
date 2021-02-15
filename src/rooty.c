ff732eb4442605e54ffea303a153e88241062d8a
hal3002 <hal@nutsec.org>
2021-02-15 10:48:21 -0600

    Updating encryption scheme and cleaning up processing

--- src/rooty.c
+++ src/rooty.c
@@ -1,10 +1,16 @@
 #include "rooty.h"
 
-uint32_t decrypt_message(const uint8_t *data, uint8_t *decoded_data, uint32_t len, uint8_t *key) {
-   uint32_t i;
+uint32_t decrypt_message(ROOTY_MESSAGE *msg, uint32_t len) {
+    uint32_t i, j;
 
-   for(i = 0; i < len; i++) {
-      decoded_data[i] = ((data[i] ^ key[0]) ^ key[1]);
+    for(i=0, j=0; i < len - BLOCK_SIZE; i++) {
+        msg->magic[i] = (msg->magic[i] ^ msg->key[j]);
+
+        if ((j > 0) && (j % (BLOCK_SIZE - 1) == 0)) {
+            j = 0; 
+        } else { 
+            j++; 
+        }
    }
 
    return i;
