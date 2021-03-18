63efc6340b35e4a2bda17b95c259cfca3fb3e8ba
hal3002 <hal@nutsec.org>
2021-03-18 18:42:07 -0500

    Adding script to build static binaries

---
+++ extras/make_static.sh
@@ -0,0 +1,10 @@
+cd ..
+wget https://www.tcpdump.org/release/libpcap-1.10.0.tar.gz
+tar -xf libpcap-1.10.0.tar.gz
+cd libpcap-1.10.0
+./configure --disable-shared --disable-dbus
+make -j
+cd ..
+
+gcc -static -FPIC -O2 -o rooty rooty_unix.c rooty.c -I libpcap-1.10.0/pcap -I libpcap-1.10.0 -Llibpcap-1.10.0 -lpcap -lpthread
+strip rooty
